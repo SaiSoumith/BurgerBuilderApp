14fca1934cdade40a0569148d376dbd9ca928c2b
Sai Krishna Soumith <saikrishnasoumith@LAP-C02ZCD71L40Y.local>
2020-12-25 13:36:51 +0530

    mac to git

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Burger from
+import Button from
+
+
+const checkoutSummary = () => {
+    return (
+        <div>
+            <Burger/>
+            <Button></Button>
+            <Button></Button>
+        </div>
+    );
+};
+
+export default checkoutSummary;
